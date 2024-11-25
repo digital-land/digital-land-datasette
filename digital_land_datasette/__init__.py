053aa32ac19bba716d17b6f98bc83a27b6a9884e
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2024-11-25 19:04:37 +0000

    try to do nothing if an error is raised

--- digital_land_datasette/__init__.py
+++ digital_land_datasette/__init__.py
@@ -1,5 +1,5 @@
 from datasette import hookimpl
-
+import logging
 PLUGIN_NAME = 'digital-land-datasette'
 
 @hookimpl
@@ -17,15 +17,18 @@ def startup(datasette):
     monkey_patch()
 
     for db_name, options in config.items():
-        if not 'directory' in options and not 'file' in options:
-            raise Exception('digital-land-datasette: expected directory or file key for db {}'.format(db))
+        try:
+            if not 'directory' in options and not 'file' in options:
+                raise Exception('digital-land-datasette: expected directory or file key for db {}'.format(db))
 
-        if 'directory' in options:
-            directory = options['directory']
-            db = DuckDatabase(datasette, directory=directory,httpfs = options['httpfs'], watch=options.get('watch', False) == True, db_name=db_name)
-            datasette.add_database(db, db_name)
-        else:
-            file = options['file']
-            db = DuckDatabase(datasette, file=file,httpfs = options['httpfs'])
-            datasette.add_database(db, db_name)
+            if 'directory' in options:
+                directory = options['directory']
+                db = DuckDatabase(datasette, directory=directory,httpfs = options['httpfs'], watch=options.get('watch', False) == True, db_name=db_name)
+                datasette.add_database(db, db_name)
+            else:
+                file = options['file']
+                db = DuckDatabase(datasette, file=file,httpfs = options['httpfs'])
+                datasette.add_database(db, db_name)
+        except Exception as e:
+            logging.error(f"digital_land_datasette raised error {e}")
 
